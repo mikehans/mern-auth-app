c91ca2b8bc3de21ff0c6544fdcfea7cdc9e72234
Mike Hansford <mike.hansford@bjss.com>
2023-07-03 14:30:32 +1000

    remove commented code

--- scripts/makeDeploy.js
+++ scripts/makeDeploy.js
@@ -1,14 +1,7 @@
-import path from 'path';
 import {mkdir, copyFile, cp} from 'fs/promises';
 
-// create a deploy folder
-// copy . to deploy
-// copy ./backend to deploy
-// copy ./frontend/dist to deploy
 try {
     await mkdir('deploy');
-
-    // await chmod('deploy', 666 );
 
     await copyFile('package.json', 'deploy/package.json');
 
@@ -19,12 +12,6 @@ try {
     await cp('node_modules', 'deploy/node_modules', {recursive: true});
 
     await cp('frontend/dist', 'deploy/frontend/dist', {recursive: true});
-
-
-    // await chmod('deploy/backend', 666 );
-    // await copyFile('backend', 'deploy/backend');
-
-    // await copyFile ('frontend/dist', 'deploy/frontend/dist');
     
 } catch (err){
     console.error(err);
